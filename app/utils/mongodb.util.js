3d3c840fffd25975a3b23767feff55ed2ae0d1f0
irzko <tranminhkha.1003@gmail.com>
2023-02-21 17:09:17 +0700

    Cai dat cac handler truy xuat CSDL

--- app/utils/mongodb.util.js
+++ app/utils/mongodb.util.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 
 class MongoDB {
-  static connect = async (url) => {
+  static connect = async (uri) => {
     if (this.client) return this.client;
     this.client = await MongoClient.connect(uri);
     return this.client;
--- server.js
+++ server.js
@@ -16,3 +16,4 @@ async function startServer() {
     process.exit();
   }
 }
+startServer();
